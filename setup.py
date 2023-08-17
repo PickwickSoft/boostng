ecee4c62fa9ba72f0085283b69109a156302aa60
Stefan Garlonta <70283087+garlontas@users.noreply.github.com>
2023-08-17 12:02:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+# Read the contents of README.md
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name='ngboost',
     version='0.1',
-    scripts=['ngboost']
+    scripts=['ngboost'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'  # Specify that it's Markdown content
 )
