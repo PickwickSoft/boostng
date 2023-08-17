077eda29200579902d01b48a5ea8f1c231a490bb
Stefan Garlonta <70283087+garlontas@users.noreply.github.com>
2023-08-17 13:37:53 +0200

    Change script/package name to boostng

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name='ngboost',
+    name='boostng',
     version='0.1',
-    scripts=['ngboost'],
+    scripts=['boostng'],
     long_description=long_description,
     long_description_content_type='text/markdown'  # Specify that it's Markdown content
 )
